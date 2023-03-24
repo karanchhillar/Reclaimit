d8d0192f9c13c3c0a8d8a6b6aaff15522a645d92
madhav <guptamadhav137@gmail.com>
2023-03-24 16:03:43 +0530

    order date

---
+++ app/src/main/java/com/example/e_waste/model/CollectionModel.kt
@@ -0,0 +1,12 @@
+package com.example.e_waste.model
+
+data class CollectionModel(
+
+    val iteml : String? = "",
+    val user_address : String? = "",
+    val user_pincode : String? = "",
+    val coll_date : String? = "",
+    val uid : String? = "",
+    val id : String? = ""
+
+)
