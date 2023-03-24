1cb52d75606f581d2e636e0cb8d34e769613765d
madhav <guptamadhav137@gmail.com>
2023-03-24 21:06:24 +0530

    data = 24/03 time = 21/06

--- app/src/main/java/com/example/e_waste/OrderFragment.kt
+++ app/src/main/java/com/example/e_waste/OrderFragment.kt
@@ -1,27 +1,46 @@
 package com.example.e_waste
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.e_waste.model.CollectionModel
 import com.example.e_waste.orders.OrdersAdapter
-
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.security.AuthProvider
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 //hlobhai
 class OrderFragment : Fragment() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var checkoutButton: Button
-    private lateinit var deleteButton: Button
-    private lateinit var orderAdapter: OrdersAdapter
+    private lateinit var auth : FirebaseAuth
+    private lateinit var database : FirebaseDatabase
+    private lateinit var storageRef: StorageReference
+    private lateinit var firebaseFirestore: FirebaseFirestore
 
+
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,16 +49,102 @@ class OrderFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_order, container, false)
 
-//        recyclerView = view.findViewById(R.id.your_order_recycler_view)
-        // codde here
+        val checkoutButton = view.findViewById<Button>(R.id.checkout_button)
+        val list = view.findViewById<EditText>(R.id.itemlist)
+        val address = view.findViewById<EditText>(R.id.sendaddress)
+        val pincode = view.findViewById<EditText>(R.id.pincode)
+        val collection_date = view.findViewById<TextView>(R.id.dateofc)
+        val coll_btn = view.findViewById<Button>(R.id.coll_date_btn)
+        auth = FirebaseAuth.getInstance()
+        storageRef = FirebaseStorage.getInstance().reference.child("COLLECTION DETAILS")
+        firebaseFirestore = FirebaseFirestore.getInstance()
 
-        checkoutButton = view.findViewById(R.id.checkout_button)
+
+        checkoutButton.setOnClickListener {
+
+            val items = list.text.toString()
+            val collec_add = address.text.toString()
+            val pin = pincode.text.toString()
+            val coll_date = collection_date.text.toString()
 
 
-        // code here
-        deleteButton = view.findViewById(R.id.delete_button)
+            database = FirebaseDatabase.getInstance()
+
+            val detail = CollectionModel(
+                items,
+                collec_add,
+                pin,
+                coll_date,
+                auth.uid.toString(),
+                System.currentTimeMillis().toString()
+
+            )
+            database.reference.child("COLLECTION DEATILS").child(System.currentTimeMillis().toString()).setValue(detail)
+                .addOnSuccessListener {
+                    Toast.makeText(activity, "YOUR COLLECTION IS CONFIRMED \n CONTACT YOU SOON", Toast.LENGTH_LONG).show()
+
+
+
+
+
+
+
+                }
+//            homeactivity2170
+        }
+
+        val calenderdate = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dateofMonth ->
+
+
+            calenderdate.set(Calendar.YEAR,year)
+            calenderdate.set(Calendar.MONTH,month)
+            calenderdate.set(Calendar.DAY_OF_MONTH,dateofMonth)
+
+            updatedatelabel(calenderdate,collection_date)
+
+        }
+
+
+
+
+        coll_btn.setOnClickListener {
+
+            DatePickerDialog(
+                requireContext(),
+                datePicker,
+                calenderdate.get(Calendar.YEAR),
+                calenderdate.get(Calendar.MONTH),
+                calenderdate.get(Calendar.DAY_OF_MONTH),
+
+
+                ).show()
+//            updatedatelabel(calenderdate,collection_date)
+
+        }
+//        updatedatelabel(calenderdate,collection_date)
+//        deleteButton = view.findViewById(R.id.delete_button)
         return view
     }
+
+
+
+
+
+
+    }
+
+    @SuppressLint("SimpleDateFormat", "SetTextI18n")
+    private fun updatedatelabel(calenderdate: Calendar, coll_date : TextView) {
+
+        val day = SimpleDateFormat("dd").format(calenderdate.time)
+        val month = SimpleDateFormat("MM").format(calenderdate.time)
+        val year = SimpleDateFormat("yyyy").format(calenderdate.time)
+        coll_date.text = "${day}/${month}/${year}"
+
+
+    }
+
 //    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 //        super.onViewCreated(view, savedInstanceState)
 //
@@ -66,4 +171,3 @@ class OrderFragment : Fragment() {
 //    }
 //}
 
-}
