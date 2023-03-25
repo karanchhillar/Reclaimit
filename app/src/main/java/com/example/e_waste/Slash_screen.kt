07e1006ec3304d6f04f4cf3f7974f590d6075e92
madhav <guptamadhav137@gmail.com>
2023-03-25 22:46:54 +0530

    data = 25/03 time = 22-46

---
+++ app/src/main/java/com/example/e_waste/Slash_screen.kt
@@ -0,0 +1,11 @@
+package com.example.e_waste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Slash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slash_screen)
+    }
+}
